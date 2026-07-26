7ee937e824d9fa6b6dd390061e126b7e0d44a71c
emilyvanderwall <emilyv.browning@gmail.com>
2026-07-26 15:22:09 -0400

    Update check.py

--- check.py
+++ check.py
@@ -1,18 +1,7 @@
 import os
-import requests
 from playwright.sync_api import sync_playwright
 
-WEBHOOK = os.environ["DISCORD_WEBHOOK"]
-
 URL = "https://www.sharkvalleytramtours.com/event-calendar/"
-
-
-def send_alert(message):
-    requests.post(
-        WEBHOOK,
-        json={"content": message},
-        timeout=30
-    )
 
 
 with sync_playwright() as p:
@@ -28,51 +17,26 @@ with sync_playwright() as p:
 
     page.wait_for_timeout(5000)
 
-    # Close popup
     try:
         page.locator(".pum-close").click(timeout=5000)
         page.wait_for_timeout(2000)
     except:
         pass
 
-    # Move July -> November
     for i in range(4):
         page.locator(".fc-text-arrow").nth(1).click(timeout=10000)
-        page.wait_for_timeout(3000)
+        page.wait_for_timeout(2000)
 
-    # Extract calendar event information
     events = page.locator(".fc-event")
 
-    found = False
+    print("Number of events:", events.count())
 
     for i in range(events.count()):
-        event = events.nth(i).inner_text()
+        event = events.nth(i)
 
-        if "2:00PM Tram Tour" in event:
-            print(event)
-
-            # Get date from event element
-            date = events.nth(i).get_attribute("data-date")
-
-            print("DATE:", date)
-
-            if date == "2026-11-07":
-
-                found = True
-
-                if "Sold Out (0)" in event:
-                    print("November 7 2 PM still sold out")
-                else:
-                    send_alert(
-                        "🚨 Shark Valley Tram Tour OPEN!\n\n"
-                        "November 7, 2026 at 2:00 PM\n\n"
-                        + URL
-                    )
-
-                break
+        if "2:00PM Tram Tour" in event.inner_text():
+            print("\n--- EVENT HTML ---")
+            print(event.evaluate("(el) => el.outerHTML"))
+            break
 
     browser.close()
-
-
-if not found:
-    print("November 7 2 PM event not found")
