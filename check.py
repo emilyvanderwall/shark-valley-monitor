03b720cedc29337a84928c6d82539823904c3a6d
emilyvanderwall <emilyv.browning@gmail.com>
2026-07-26 14:33:20 -0400

    Update check.py

--- check.py
+++ check.py
@@ -1,1 +1,13 @@
-print("Hello! GitHub is running my script.")
+import os
+import requests
+
+webhook = os.environ["DISCORD_WEBHOOK"]
+
+requests.post(
+    webhook,
+    json={
+        "content": "✅ Shark Valley monitor is connected! Discord alerts are working."
+    }
+)
+
+print("Discord message sent!")
